efafb78789b868bafa906aa5531f610185df047d
Tatina Gololobova <tgololobova@inbox.ru>
2022-08-20 10:26:51 +0300

    remove lesson_3-2.py

--- lesson_3-2.py
+++
@@ -1,8 +0,0 @@
-def user_info(first_name, last_name, year, city, email, phone_number):
-    print(f"{first_name} {last_name}, {year} года рождения, город проживания - {city}, email: "
-          f"{email}, номер телефона: "
-          f"{phone_number}")
-
-
-print(user_info(first_name="Татьяна", last_name="Петрова", year="1992", city="Москва",
-                email="tatiana@mail.ru", phone_number="4951234567"))
